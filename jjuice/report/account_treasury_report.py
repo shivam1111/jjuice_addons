e4c23efe974028250dae464d881ac2799073c061
Shivam Goyal <shivam1111@gmail.com>
2017-01-10 18:23:26 +0530

    Added source acquisition report in treasury analysis

--- jjuice/report/account_treasury_report.py
+++ jjuice/report/account_treasury_report.py
@@ -12,6 +12,12 @@ ACCOUNT_TYPE  = [('smoke_shop',"Smoke Shop"),('vape_shop','Vape Shop'),('conveni
                  ('website','Online Store'),
                  ]
 
+_SOURCE = [
+           ('trade_show','Trade Show'),
+           ('sales_trip','Sales Trip'),
+           ('magazine_add','Magazine Add'),
+           ('referral','Referral'),
+           ]
 
 class account_treasury_report(osv.osv):
     _inherit = "account.treasury.report"
@@ -21,7 +27,9 @@ class account_treasury_report(osv.osv):
     _columns = {
                 'partner_id':fields.many2one('res.partner','Partner',readonly=True),
                 'account_type':fields.selection(ACCOUNT_TYPE,string = "Type Of Account",readonly=True),
-                'classify_finance':fields.selection(FINANCE_CLASSIFY,string = "Account Classification(For Finance)",readonly=True)
+                'classify_finance':fields.selection(FINANCE_CLASSIFY,string = "Account Classification(For Finance)",readonly=True),
+                'source':fields.selection(_SOURCE,string="Basic Acquisition Source",readonly=True),
+                'source_name':fields.char("Acquisition Source",readonly=True),                
                 }
     
     def init(self, cr):
@@ -34,6 +42,8 @@ class account_treasury_report(osv.osv):
                 p.id as period_id,
                 COALESCE (partner.acccount_type, 'No Label') as account_type ,
                 COALESCE (partner.classify_finance, 'No Finance Classification') as classify_finance,
+                aq.name as source_name,
+                aq.source as source,
                 l.partner_id as partner_id,
                 sum(l.debit) as debit,
                 sum(l.credit) as credit,
@@ -46,9 +56,12 @@ class account_treasury_report(osv.osv):
                 left join account_move am on (am.id=l.move_id)
                 left join account_period p on (am.period_id=p.id)
                 left join res_partner partner on (l.partner_id = partner.id)
+                left join account_acquisition aq on aq.id = partner.acquisition_id
             where l.state != 'draft'
               and a.type = 'liquidity'
-            group by p.id, p.fiscalyear_id, p.date_start, am.company_id,l.partner_id,partner.acccount_type,partner.classify_finance
+            group by p.id, p.fiscalyear_id, p.date_start, am.company_id,l.partner_id,partner.acccount_type,
+            partner.classify_finance,aq.name,
+                    aq.source
             )
         """)
 
